b5946ce457aaef5a5d5af1b5a72c79fcfaaaffb0
Mie O <miei1@outlook.com>
2017-04-25 17:34:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,21 +52,23 @@ module.exports = function Relog(dispatch) {
 	function relog(targetId) {
 		if (!targetId) return
 		dispatch.toServer('C_RETURN_TO_LOBBY', 1, {})
-
+		let userListHook
+		let lobbyHook
+		
 		// make sure that the client is able to log out
 		const prepareLobbyHook = hookNext('S_PREPARE_RETURN_TO_LOBBY', 1, event => {
 			dispatch.toClient('S_RETURN_TO_LOBBY', 1, {})
-		})
-		
-		// the server is not ready yet, displaying "Loading..." as char names
-		const userListHook = hookNext('S_GET_USER_LIST', 1, event => {
-			event.characters.forEach(char => char.name = 'Loading...')
-			return true
-		})
 
-		// the server is ready to relog to a new character
-		const lobbyHook = hookNext('S_RETURN_TO_LOBBY', 1, event => {
-			dispatch.toServer('C_SELECT_USER', 1, { id: targetId, unk: 0 })
+			// the server is not ready yet, displaying "Loading..." as char names
+			userListHook = hookNext('S_GET_USER_LIST', 1, event => {
+				event.characters.forEach(char => char.name = 'Loading...')
+				return true
+			})
+
+			// the server is ready to relog to a new character
+			lobbyHook = hookNext('S_RETURN_TO_LOBBY', 1, event => {
+				dispatch.toServer('C_SELECT_USER', 1, { id: targetId, unk: 0 })
+			})
 		})
 
 		// hook timeout, in case something goes wrong
